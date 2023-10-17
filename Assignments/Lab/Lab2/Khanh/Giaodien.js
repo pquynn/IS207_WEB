e0ee99e1eaca17f00169c7a37e95b0057818ed39
Nhu Khanh <21522204@gm.uit.edu.vn>
2023-10-17 14:42:54 +0700

    lab2 may tinh

---
+++ Assignments/Lab/Lab2/Khanh/Giaodien.js
@@ -0,0 +1,18 @@
+/** @format */
+
+// select number
+const numberArray = document.querySelectorAll(".number");
+
+// select operator
+const operator = document.querySelectorAll(".operation");
+
+// digit
+const digit = document.querySelector(".digit");
+
+// CE, DEL button
+const ce = document.querySelector(".ce");
+const del = document.querySelector(".del");
+
+//input, output
+const input = document.querySelector(".input-line input");
+const output = document.querySelector(".out-line input");
