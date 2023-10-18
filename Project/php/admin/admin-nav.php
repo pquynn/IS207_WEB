a901f5e781a1bbeb74a14ea73431e75e177711ab
Phuong Quyen <pquyen245@gmail.com>
2023-10-18 22:41:31 +0700

    update product-management files
    + change html into php and cut layout
    + change css style

---
+++ Project/php/admin/admin-nav.php
@@ -0,0 +1,46 @@
+<!-- cut layout: admin navigation. modified 10/18/2023 by Quyen -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/f7fcb1a9ac.js"crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../css/base.css">
+    <link rel="stylesheet" href="../../css/admin-navigation.css">
+    <link rel="stylesheet" href="../../css/table.css">
+    <link rel="stylesheet" href="../../css/pagination.css">
+    <link rel="stylesheet" href="../../css/products-management.css">
+    <title><?php echo $title; ?></title>
+
+</head>
+<body>
+    <!-- start: admin navigation -->
+    <div class="nav-container">
+        <div class="nav-admin">
+            <a href="#" class="nav-admin-logo">
+                <!-- change logo color into white. modified 10/18/2023 by Quyen -->
+                <img src="../../img/logo-white.png" alt="logo"> 
+                <h1>LOGO</h1>
+            </a>
+    
+            <ul class="nav-admin-menu">
+                <li><a href="#">Về Cửa Hàng</a></li>
+                <li><a href="#">Thống Kê</a></li>
+                <li><a href="#">Danh Mục</a></li>
+                <li><a href="#" class="active">Sản Phẩm</a></li>
+                <li><a href="#">Đơn Hàng</a></li>
+                <li><a href="#">Khách Hàng</a></li>
+                <li><a href="#">Nhân Viên</a></li>
+                <li><a href="#">Blog</a></li>
+            </ul>
+    
+            <div class="nav-admin-account">
+                <a href="#" id="account">
+                        <img src="../../img/user.png" alt="Your account">
+                        <p>Nguyễn Văn A</p>
+                </a>
+                <a href="#" id="logout">Đăng Xuất</a>
+            </div>
+        </div>  
+    </div>
+<!-- end: admin navigation -->
