201ef7ce0dc53fd13877f68844f69a13df545314
Nhu Khanh <21522204@gm.uit.edu.vn>
2023-10-18 07:48:59 +0700

    lad 2 Khanh

--- Assignments/Lab/Lab2/Khanh/Giaodien.js
+++ Assignments/Lab/Lab2/Khanh/Giaodien.js
@@ -32,43 +32,56 @@ let expression = "";
 //Number
 numberArray.forEach((number) => {
   number.addEventListener("click", function () {
-    expression += number.textContent;
-    currentNumString += number.textContent;
-
     // display
+    expression += number.textContent;
     input.value = expression;
+
+    // get number
+    currentNumString += number.textContent;
   });
 });
 
 // Operator
 operatorArray.forEach((operator) => {
   operator.addEventListener("click", function () {
+    // display
     expression += operator.textContent;
     input.value = expression;
 
+    // get operator
     firstNum = Number(currentNumString);
     currentOperator = operator.textContent;
     currentNumString = "";
   });
 });
 
+// decimal-point
+decimalPoint.addEventListener("click", function () {
+  // display
+  expression += ".";
+  input.value = expression;
+
+  // get decimal-point
+  currentNumString += ".";
+});
+
 // equal
 equal.addEventListener("click", function () {
+  // get second num
   secondNum = Number(currentNumString);
 
   let result = 0;
 
+  // caculate
   result = currentOperator === "+" ? firstNum + secondNum : result;
-  result = currentOperator === "-" ? firstNum + secondNum : result;
-  result = currentOperator === "*" ? firstNum + secondNum : result;
-  result = currentOperator === "/" ? firstNum + secondNum : result;
+  result = currentOperator === "-" ? firstNum - secondNum : result;
+  result = currentOperator === "*" ? firstNum * secondNum : result;
+  result = currentOperator === "/" ? firstNum / secondNum : result;
 
   output.value = result;
-
-  // }
 });
 
-// ce
+// ce (delete all)
 ce.addEventListener("click", function () {
   input.value = output.value = 0;
   firstNum = secondNum = 0;
