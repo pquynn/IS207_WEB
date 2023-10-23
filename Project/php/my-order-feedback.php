afc82e6d12a0cb89a6094260c718a06422d02a5f
Ngoc Anh <21520584@gm.uit.edu.vn>
2023-10-23 11:07:46 +0700

    my order feedback

---
+++ Project/php/my-order-feedback.php
@@ -0,0 +1,35 @@
+<?php
+    include("header.php");
+?>
+<!--MY_ORDER-FEEDBACK START-->
+<div class="my-order-feedback">
+      <h1>Chi tiết đơn hàng #A123 > Đánh giá đơn hàng</h1>
+        <p class="star product">
+          Đánh giá sản phẩm
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </p>
+
+        <p class="star service">
+          Đánh giá dịch vụ người bán
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+          <i class="fa-regular fa-star"></i>
+        </p>
+
+        <textarea placeholder="Nhận xét đơn hàng" class="input-feedback"></textarea>
+
+        <input type="submit" value="Đánh giá" class="submit-feedback"> 
+
+    </div>
+</div>
+<!--MY_ORDER-FEEDBACK END-->
+
+<?php
+    include("footer.php");
+?>
