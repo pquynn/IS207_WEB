27d955f8a511dc400d7ad1fb0ca25ff70a98756f
Ngoc Anh <21520584@gm.uit.edu.vn>
2023-10-26 09:45:05 +0700

    Update admin-orders
    - add $title.
    - add breadcrumb.
    - data modified.
    - table' style -> table.css.
    - pagination class.

--- Project/php/admin/admin-orders.php
+++ Project/php/admin/admin-orders.php
@@ -1,10 +1,13 @@
 <?php
-    include("admin-order-navigation.php");
+    $title = "Danh sách đơn hàng";
+    include("admin-nav.php");
 ?>
 
 <!--MAIN SECTION----START-->
 <div class="admin-orders">
-    <h2>Danh sách đơn hàng</h1>
+    <ol class="breadcrumb">
+        <li class= "breadcrumb-item active" aria-current="page">Danh sách đơn hàng</li>
+    </ol>
     <!--Orders' date-->
     <div class="date-orders">
         <i class="fa-solid fa-calendar"></i>
@@ -31,30 +34,36 @@
 
     <!--Table list orders-->
     <table class="list-orders">
-        <th colspan="7">Danh sách đơn hàng</th>
+        <th colspan="10">Danh sách đơn hàng</th>
         <tr>
             <td><input type="checkbox"></td>   
-            <td>Mã đơn hàng</td>   
-            <td>Ngày</td>   
-            <td>PTTH</td>   
-            <td>Tên khách hàng</td>   
+            <td>Mã đơn hàng</td>
+            <td>Ngày hóa đơn</td>   
+            <td>SĐT</td>   
+            <td>Tên khách hàng</td>                  
+            <td>Địa chỉ</td>
+            <td>PTTT</td>   
             <td>Trạng thái</td>   
-            <td>Trị giá</td>   
+            <td>Tổng sản phẩm</td>   
+            <td>Tổng tiền</td>   
         </tr>
 
         <tr>
             <td><input type="checkbox"></td>
-            <td>A123</td>   
-            <td>dd/mm/yyyy</td>   
+            <td>A123</td>
+            <td>dd/mm/yyy</td>   
+            <td>0912345678</td>   
+            <td>ABC DEF GHIJK</td>                  
+            <td>123 ABCD</td>
             <td>Momo</td>   
-            <td>ABCD EFGH IJKLMN</td>   
             <td>Giao hàng thành công</td>   
-            <td>$9999</td>   
+            <td>3</td>   
+            <td>$9999</td> 
         </tr>
     </table>
 
     <!--Pagination-->
-    <div class="pagination">
+    <div class="pagination admin">
         <a href="#">&laquo;</a>
         <a class="active" href="#">1</a>
         <a href="#">2</a>
