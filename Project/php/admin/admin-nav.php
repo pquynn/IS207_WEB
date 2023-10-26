b6659ae7e4932cb88bbeb56bef4920c3a040dd63
Ngoc Anh <21520584@gm.uit.edu.vn>
2023-10-26 09:34:34 +0700

    Update admin-nav

--- Project/php/admin/admin-nav.php
+++ Project/php/admin/admin-nav.php
@@ -9,9 +9,19 @@
     <link rel="stylesheet" href="../../css/admin-navigation.css">
     <link rel="stylesheet" href="../../css/table.css">
     <link rel="stylesheet" href="../../css/pagination.css">
-    <link rel="stylesheet" href="../../css/products-management.css">
     <title><?php echo $title; ?></title>
 
+    <!--css feat-->
+    <link rel="stylesheet" href="../../css/admin-orders.css"/>
+
+    <link rel="stylesheet" href="../../css/admin-order-detail.css"/>
+    <!--icon-->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">  
 </head>
 <body>
     <!-- start: admin navigation -->
@@ -27,8 +37,8 @@
                 <li><a href="#">Về Cửa Hàng</a></li>
                 <li><a href="#">Thống Kê</a></li>
                 <li><a href="#">Danh Mục</a></li>
-                <li><a href="#" class="active">Sản Phẩm</a></li>
-                <li><a href="#">Đơn Hàng</a></li>
+                <li><a href="#">Sản Phẩm</a></li>
+                <li><a href="#" class="active">Đơn Hàng</a></li>
                 <li><a href="#">Khách Hàng</a></li>
                 <li><a href="#">Nhân Viên</a></li>
                 <li><a href="#">Blog</a></li>
