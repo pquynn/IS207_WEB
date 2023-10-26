70a431b41d57ff3ebbbda469af44454439af72f3
Ngoc Anh <21520584@gm.uit.edu.vn>
2023-10-26 21:31:43 +0700

    other updates
    -pagination.css
    -header.php

--- Project/php/header.php
+++ Project/php/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Đơn hàng của tôi</title>
+    <title><?php echo $title; ?></title>
 
     <!-- css file general-->
     <link rel="stylesheet" href="../css/base.css"/>
@@ -22,7 +22,6 @@
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
     <!---->  
 
     <script src="https://kit.fontawesome.com/f7fcb1a9ac.js"crossorigin="anonymous"></script>
