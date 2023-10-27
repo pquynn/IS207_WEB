3d2dbb2df6eac653dd398e0802318ef83d059783
Phuong Quyen <pquyen245@gmail.com>
2023-10-27 19:55:40 +0700

    change admin navigation file name

--- Project/php/admin/AdminNavigation.php
+++ Project/php/admin/AdminNavigation.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
     <link rel="stylesheet" href="../../css/base.css">
-    <link rel="stylesheet" href="../../css/admin-navigation.css">
+    <link rel="stylesheet" href="../../css/AdminNavigation.css">
     <link rel="stylesheet" href="../../css/table.css">
     <link rel="stylesheet" href="../../css/pagination.css">
     <link rel="stylesheet" href="../../css/admin/ProductsManagement.css">
