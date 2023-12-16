db9a52b1d6f35d4c5f8b2367898020eca5285ba3
lenguyenhoanghuy253 <104979551+lenguyenhoanghuy253@users.noreply.github.com>
2023-12-16 23:26:02 +0700

    Create Login.js

---
+++ Project/js/storemy-order/Login.js
@@ -0,0 +1,38 @@
+function validateLogin() {
+  var userlogin = document.getElementById("userlogin").value;
+  var pass = document.getElementById("pass").value;
+
+  // gửi dữ liệu đăng nhập lên server
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "php/login.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  var data = "userlogin=" + encodeURIComponent(userlogin) + "& pass=" + encodeURIComponent(pass);
+  xhr.send(data);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var response = JSON.parse(xhr.responseText);
+      if (response.status === "success") {
+        //Phân quyền
+        var role = response.role;
+            if (role === 'admin') {
+                // Link tới admin
+                window.location.href = "#";
+            } else if (role === 'staff') {
+                // Link tới staff
+                window.location.href = "#";
+            } else {
+                // Link tới User
+                window.location.href = "#";
+            }
+        
+      } else {
+        // Đăng nhập thất bại, hiển thị thông báo lỗi
+        document.getElementById("messageContainer").innerHTML =
+          '<p class="error-message">' + response.message + "</p>";
+      }
+    }
+  };
+  return false;
+}
