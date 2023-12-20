951bf2d25258b5884a4d3e7aa7ec2d4cf323e8ea
Nhu Khanh <21522204@gm.uit.edu.vn>
2023-12-20 14:46:48 +0700

    update cart

--- Project/php/store/cart/cartDisplayProduct.php
+++ Project/php/store/cart/cartDisplayProduct.php
@@ -12,7 +12,7 @@ function fetchProducts() {
                 ON order_detail.ORDER_ID = orders.ORDER_ID
                 INNER JOIN product_pictures
                 ON order_detail.product_id = product_pictures.product_id
-          WHERE orders.ORDER_ID=4";
+          -- WHERE orders.ORDER_ID=4";
   
   $orderDetailList = $conn->query($sql);
   //   SELECT TABLE: END
