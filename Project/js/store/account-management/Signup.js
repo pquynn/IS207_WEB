19ec065863b1a5c41760812b30cea580797a30c4
Phuong Quyen <pquyen245@gmail.com>
2023-12-20 12:40:54 +0700

    update login, signup validation message

--- Project/js/store/account-management/Signup.js
+++ Project/js/store/account-management/Signup.js
@@ -2,19 +2,21 @@ import { showToastr } from "../../admin/toastr.js";
 
 $(document).ready(function () {
     $('.btn-confirm').click(function (event){
-        event.preventDefault();
         // Lấy giá trị từ các trường nhập liệu
         var name = document.getElementById('name').value;
         var customerphone = document.getElementById('customerphone').value;
         var userlogin = document.getElementById('userlogin').value;
         var password = document.getElementById('password').value;
         var repassword = document.getElementById('repassword').value;
+        var input_phone = document.getElementById('customerphone');
 
         if(name.localeCompare('') != 0 
         && customerphone.localeCompare('') != 0
         && userlogin.localeCompare('') != 0
         && password.localeCompare('') != 0
-        && repassword.localeCompare('') != 0){
+        && repassword.localeCompare('') != 0
+        && input_phone.checkValidity()){
+            event.preventDefault();
             // Kiểm tra mật khẩu nhập lại
             if (password !== repassword) {
                 showToastr('error', 'Mật khẩu không khớp. Vui lòng nhập lại.');
