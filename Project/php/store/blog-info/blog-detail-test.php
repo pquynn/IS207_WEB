9d004bca7026f8af517e51fe71fd89f0fd5186e4
Phuong Quyen <pquyen245@gmail.com>
2023-12-22 10:43:05 +0700

    update code

--- Project/php/store/blog-info/blog-detail-test.php
+++ Project/php/store/blog-info/blog-detail-test.php
@@ -1,3 +1,8 @@
+<?php session_start();
+$user_id = isset($_SESSION['user_id']) ? json_encode($_SESSION['user_id']) : 'null';
+echo '<script> var user_id =' . $user_id . ';</script>';
+?>
+
 <link rel="stylesheet" href="../../../css/store/blog-detail.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
